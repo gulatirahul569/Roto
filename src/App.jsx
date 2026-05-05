9a83240187b64bcf44c910b9bae7271b8ac2fabf
somia kumari <Kumarisomiak@gmail.com>
2026-05-05 17:54:18 +0530

    made the routes of all the pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,39 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import New from './Mainlinks/New'
+import Bags from './Mainlinks/Bags'
+import HeaderSlider from './Components/HeaderSlider'
 import Navbar from './Components/Navbar'
 import Section1 from './Pages/Section1'
 import Section2 from './Pages/Section2'
-import HeaderSlider from './Components/HeaderSlider'
+import Shoes from './Mainlinks/Shoes'
+import Accessories from './Mainlinks/Accessories'
+import Sling from './Mainlinks/Sling'
+import Sale from './Mainlinks/Sale'
 
 const App = () => {
   return (
-    
-    <div>
+    <BrowserRouter>
       <HeaderSlider />
       <Navbar />
-      <Section1 />
-      <Section2 />
-    </div>
+
+      <Routes>
+        <Route path="/" element={
+          <>
+            <Section1 />
+            <Section2 />
+          </>
+        } />
+
+        <Route path="/bags" element={<Bags />} />
+        <Route path="/new" element={<New />} />
+        <Route path="/shoes" element={<Shoes />} />
+        <Route path="/accessories" element={<Accessories />} />
+        <Route path="/sling" element={<Sling />} />
+        <Route path="/sale" element={<Sale />} />
+      </Routes>
+
+    </BrowserRouter>
   )
 }
 
--- src/Pages/Section2.jsx
+++ src/Pages/Section2.jsx
@@ -1,13 +1,71 @@
 import React from 'react'
+import { CiCircleChevRight } from "react-icons/ci";
 
 const Section2 = () => {
   return (
-    <div className='flex flex-col '>
+    <div className='flex flex-col'>
+
       <div className='flex justify-center items-center font-bold uppercase text-4xl p-8'>
         Made for What’s Ahead. Since 1995.
       </div>
 
+      <div className='flex justify-between px-12' >
 
+        {/* Card */}
+        <div className='relative group cursor-pointer overflow-hidden'>
+          <img
+            className='h-92 group-hover:scale-105 transition duration-300  '
+            src="https://chromeindustries.com/cdn/shop/files/Kadet_Max_Steel_Blue_SQ_HP_1.jpg?v=1777073141&width=1200"
+            alt=""
+          />
+
+          {/* Always visible text */}
+          <div className='absolute bottom-4 left-4 p-2.5 pr-3.5 pl-3.5 bg-white '>
+            <button className='text-xl font-bold '>Shop Bags</button>
+          </div>
+
+          {/* Icon (appears on hover) */}
+          <div className='absolute top-4 right-4 opacity-0 group-hover:opacity-100 transition duration-300'>
+            <div className='bg-white p-1 rounded-full'>
+              <CiCircleChevRight className='text-black text-3xl' />
+            </div>
+          </div>
+        </div>
+
+        {/* Repeat for others */}
+        <div className='relative group cursor-pointer overflow-hidden'>
+          <img
+            className='h-92 group-hover:scale-105 transition duration-300'
+            src="https://chromeindustries.com/cdn/shop/files/HP_Accessories_sm_sq_2_1.jpg?v=1769108394&width=1200"
+            alt=""
+          />
+          <div className='absolute bottom-4 left-4 p-2.5 pr-3.5 pl-3.5 bg-white'>
+            <button className='text-xl font-bold '>Accessories</button>
+          </div>
+          <div className='absolute top-4 right-4 opacity-0 group-hover:opacity-100 transition duration-300'>
+            <div className='bg-white p-1 rounded-full'>
+              <CiCircleChevRight className='text-black text-3xl' />
+            </div>
+          </div>
+        </div>
+
+        <div className='relative group cursor-pointer overflow-hidden '>
+          <img
+            className='h-92 group-hover:scale-105 transition duration-300 '
+            src="https://chromeindustries.com/cdn/shop/files/hp_pack_it_up_20L_1.jpg?v=1777073743&width=1200"
+            alt=""
+          />
+          <div className='absolute bottom-4 left-4 p-2.5 pr-3.5 pl-3.5 bg-white'>
+            <button className='text-xl font-bold'>New Arrivals</button>
+          </div>
+          <div className='absolute top-4 right-4 opacity-0 group-hover:opacity-100 transition duration-300'>
+            <div className='bg-white p-1 rounded-full'>
+              <CiCircleChevRight className='text-black text-3xl ' />
+            </div>
+          </div>
+        </div>
+
+      </div>
     </div>
   )
 }

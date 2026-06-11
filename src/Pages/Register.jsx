23ac599033bb756cb07a0148994733b575afc5d8
gulatirahul569 <rahulgulati425@gmail.com>
2026-06-11 15:51:15 +0530

    Same bug removing

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -19,7 +19,7 @@ const Register = () => {
     e.preventDefault();
 
     try {
-      await axios.post(`${import.meta.env.VITE_API_URL}auth/register`, formData);
+      await axios.post(`${import.meta.env.VITE_API_URL}/auth/register`, formData);
 
       alert("Account created successfully");
       navigate("/login");
